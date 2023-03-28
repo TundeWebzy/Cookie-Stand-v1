89d3087c0d2e2184cbb45c0221c6c1e7fb3958ec
Babatunde Akeem <tundewisdom0200@gmail.com>
2023-03-28 01:08:42 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,26 +1,86 @@
-let myName = "Babatunde"
-console.log(myName)
+// const heading = document.getElementById("heading");
+// console.log(heading);
 
-let myAge = 30;
+// variable assigned value is a string
+// const myName = "Babatunde";
 
-const nameAge = "Hello my name is " + myName + " and I am " + myAge + " years old";
-console.log(nameAge)
+// variable assigned value is a number
+// let myAge = 30;
 
-const y = "5";
-let x = 10;
-let number = x + y;
-console.log(typeof number);
+// const nameAge = "Hello my name is " + myName + " and I am " + myAge + " years old";
 
-const demo = document.getElementById("demo");
-console.log(demo);
-demo.innerHTML = message;
+// console.log(nameAge);
 
+// variable assigned value is a boolean
+// let isAlive = true;
 
+// variable assigned value is a array
+// const myPets = [{}, {}, {}];
 
+// variabel assigned value is a object
+// const locations = {
+//    heading: "",
+//    location: "",
+//    highlyRated: true,
+//    price: 48,
+// };
 
+// let x = 5 + 5;
+// let y = 6 - 3;
+// let a = 2 * 2;
+// let b = 5 / 5;
 
-// for{initialization; consition; increment}{
-// statements
+// let myHouseNum = myName + " " + "is showing us some javascript";
+// console.log(myName);
+
+// let x = 5;
+// const y = "5";
+
+// let number = x + y;
+// console.log(typeof number);
+
+// const demo = document.getElementById("demo");
+// console.log(demo);
+// demo.innerHTML = message;
+
+// document.getElementById("demo").innerHTML = message;
+
+// for(initialization; condition; increment){
+//    statements
 // }
 
-// string
+// string - sentence "Michael"
+// number - 1
+// boolen - true or false
+// array - ["lou", "victor", "jeremy"]
+// object - {name: "lou", age: 21,}
+
+let myFavPets = [Dog", "Cat", "GoldFish"];
+
+// console.log(myFavPets[0]);
+// console.log(myFavPets[1]);
+// console.log(myFavPets[2]);
+
+for (let i = 0; i < myFavPets.length; i++) {
+    console.log("I love my pet " + myFavPets[i]);
+}
+
+// while(condition){
+//    statement
+//}
+
+let n = 0;
+let x = 0;
+
+while (n < 3) {
+    n++;
+    x += n;
+    console.log(n);
+    console.log(x);
+}
+
+let numberOfClick = 0;
+
+while (numberOfClick < 5) {
+    numberOfClick++;
+}
